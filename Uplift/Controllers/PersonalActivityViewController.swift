7adc49da2c8fe48679c536b3d701ea0e4b38f699
Kevin Chen <kc3585@nyu.edu>
2019-02-17 08:59:32 -0500

    Made round profile pic, added 6 quotes and dates; not yet added images, need to make aesthetic

--- Uplift/Controllers/PersonalActivityViewController.swift
+++ Uplift/Controllers/PersonalActivityViewController.swift
@@ -11,8 +11,13 @@ import UIKit
 class PersonalActivityViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    let comm = ["Bob","Joe","Joseph"]
-    let date = ["This", "Words", "Hello"]
+    let comm = ["\"One of the most inspiring stories of my generation. Truly a gift from god\"",
+                "\"It Doesn’t Matter Where You Came From. All That Matters Is Where You Are Going\"",
+                "\"A Clear Vision, Backed By Definite Plans, Gives You A Tremendous Feeling Of Confidence And Personal Power\"",
+                "\"Success Does Not Consist In Never Making Mistakes, But In Never Making The Same One A Second Time\"",
+                "\"Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small\"",
+                "\"Success Is Getting What You Want, Happiness Is Wanting What You Get\""]
+    let date = ["-February 12", "-February 13","-February 14","-February 15","-February 16","-February 17"]
     
     // NEED these for table view to work
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
