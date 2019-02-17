9f94df59e32216de63ed04a3a92fc972835174a7
Matthew <matthew.b.rodriguez@gmail.com>
2019-02-16 23:39:36 -0500

    Add 5 View Controller classes

---
+++ Uplift/PersonalActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonalActivityViewController.swift
+//  Uplift
+//
+//  Created by Matthew Rodriguez on 2/16/19.
+//  Copyright © 2019 Matthew Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class PersonalActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
