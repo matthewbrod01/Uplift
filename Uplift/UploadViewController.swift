7baf4450112f9c6c507d52526a76407abc574f87
Matthew <matthew.b.rodriguez@gmail.com>
2019-02-16 23:24:19 -0500

    Uplink to Firebase Realtime Database

--- Uplift/UploadViewController.swift
+++ Uplift/UploadViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let ref = Database.database().reference(fromURL: "https://uplift-8ef8c.firebaseio.com/").child("a")
+        ref.updateChildValues(["someKey": 25]) { (error, ref) in
+            print("Hello World")
+        }
+        print("Hello")
         // Do any additional setup after loading the view.
     }
     
