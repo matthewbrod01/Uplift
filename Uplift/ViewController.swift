d0d95130634d6bcfe4b5f0af7b39dc439454e29d
Matthew <matthew.b.rodriguez@gmail.com>
2019-02-16 23:01:56 -0500

    Initial Commit

---
+++ Uplift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uplift
+//
+//  Created by Matthew Rodriguez on 2/16/19.
+//  Copyright © 2019 Matthew Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
